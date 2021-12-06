3c9f8fd50b0c55464ff9aeacdb4513bf1f148fa4
Alice Lai <alail5077@gmail.com>
2021-12-06 10:02:38 +0000

    Add quit action in editor environment

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -7,7 +7,11 @@ public class LevelLoader : MonoBehaviour
 {
     public void Quit()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 
     public void LoadNextScene()
